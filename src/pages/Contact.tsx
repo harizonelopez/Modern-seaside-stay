c2438ce0679f51c2f0239cebd4e50236fe063b1a
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2025-11-19 12:55:08 +0300

    fix: remove unnecessary empty line in contact form section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -156,7 +156,6 @@ export default function Contact() {
                   />
                 </div>
                 </div>
-
               </div>
               
               {/* Contact Form */}
